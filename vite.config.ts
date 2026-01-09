3d852cb8e58ada19be74c0becf3924420b12d1df
Javier <javieralbertoni@gmail.com>
2026-01-08 23:52:14 -0300

    Fix asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/cozy-garden/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'esbuild',
-    target: 'es2015'
+    target: 'es2015',
+    copyPublicDir: true
   },
   server: {
     port: 3000,
